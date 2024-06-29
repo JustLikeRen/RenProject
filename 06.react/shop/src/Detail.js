480ac0d0c2275cdcd78d0842eec5e7fb9b9d5826
DESKTOP-94SFF2U\yoon7 <yoon76487@gmail.com>
2024-06-29 09:29:33 +0900

    styled-components 만져봄

--- 06.react/shop/src/Detail.js
+++ 06.react/shop/src/Detail.js
@@ -1,4 +1,16 @@
 import { useParams } from "react-router-dom";
+import styled from "styled-components";
+
+let Box = styled.div`
+  padding : 20px;
+  color : grey
+`;
+// if문 처럼 사용가능
+let YellowBtn = styled.button`
+  background : ${ props => props.bg };
+  color : ${ props => props.bg == 'blue'? 'white' : 'black' };
+  padding : 10px;
+`;
 
 function Detail(props){
     
@@ -10,8 +22,8 @@ function Detail(props){
         return x.id == id
       });
     
-    console.log(findshoes);
     return (
+        
       <div className="container">
         <div className="row">
           <div className="col-md-6">
@@ -20,9 +32,13 @@ function Detail(props){
           <div className="col-md-6">
             <h4 className="pt-5">{[findshoes.title]}</h4>
             <p>{[findshoes.content]}</p>
-            <p>{[findshoes.price]}</p>
+            <p>{[findshoes.price]}원</p>
             <button className="btn btn-danger">주문하기</button> 
           </div>
+        </div>
+        <div>
+        <YellowBtn bg="blue">파란색 버튼임</YellowBtn>
+        <YellowBtn bg="gray">그래이색 버튼임</YellowBtn>
         </div>
       </div> 
     )
