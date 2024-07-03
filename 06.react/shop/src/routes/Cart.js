cd0e095928727473c7bd8617dd6b2f59eeda158e
DESKTOP-94SFF2U\yoon7 <yoon76487@gmail.com>
2024-07-03 17:18:40 +0900

    redux 숙제 풀면서 count 늘어나고 줄어드는거 해봄

--- 06.react/shop/src/routes/Cart.js
+++ 06.react/shop/src/routes/Cart.js
@@ -1,8 +1,17 @@
 import {Table} from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import { changeName,changePlus,changeMinus } from './../store';
 
 function Cart(){
+    
+    // a.user 이런식으로 오브젝트에서 꺼내쓰듯이 사용
+    let state = useSelector((state)=>state)
+    // store.js로 요청 보내주는 함수
+    let dispatch = useDispatch();
+    
     return (
         <div>
+            {state.user}의 장바구니
             <Table>
                 <thead>
                     <tr>
@@ -13,12 +22,29 @@ function Cart(){
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>안녕</td>
-                        <td>안녕</td>
-                        <td>안녕</td>
-                    </tr>
+                    {
+                        // 오브젝트형을 i만큼 뿌릴려면 맵으로 돌려야함 
+                        // 다른방식도 있나? 일단 이거밖에 몰라서 이거만 씀 ㅋㅋ
+                        state.buy.map((id, i) => (
+                            <tr key={id.id}>
+                                <td>{i+1}</td>
+                                <td>{id.name}</td>
+                                <td>{id.count}</td>
+                                <td>안녕</td>
+                                <td>
+                                    <button style={{marginRight : "10px"}} onClick={()=>{
+                                        dispatch(changeMinus(id.id))
+                                    }}>-</button>
+                                    <button onClick={()=>{
+                                        dispatch(changePlus(id.id))
+                                    }}>+</button>
+                                </td>
+                                
+                                   
+                                
+                            </tr>
+                        ))
+                    }
                 </tbody>
             </Table> 
         </div>
--- 06.react/shop/src/store.js
+++ 06.react/shop/src/store.js
@@ -1,7 +1,48 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, createSlice } from '@reduxjs/toolkit'
 
+//useState역할 redex수정하는 함수만들고 원할때 함수 실행해달라고 요청
+let user = createSlice({
+    name : 'user',
+    initialState : 'kim',
+    reducers : {
+        changeName(state){
+            return 'john kim'
+        }
+    }
+})
+
+export let { changeName } = user.actions
+
+let buy = createSlice({
+   name : 'buy',
+   initialState : [
+        {id : 0, name : 'White and Black', count : 2},
+        {id : 2, name : 'Grey Yordan', count : 1}
+      ],
+      reducers : {
+        changePlus(state, action){
+            // action.payload에 담긴 id로 해당 아이템을 찾습니다.
+            const item = state.find(item => item.id === action.payload);
+            if (item) {
+            // 해당 아이템의 count를 1 증가시킵니다.
+            item.count += 1;
+        }},
+        changeMinus(state, action){
+            // action.payload에 담긴 id로 해당 아이템을 찾습니다.
+            const item = state.find(item => item.id === action.payload);
+            if (item && item.count > 0) {
+            // 해당 아이템의 count를 1 증가시킵니다.
+            item.count -= 1;
+        }}
+    }})
+
+export let { changePlus, changeMinus } = buy.actions
+
+//redux 쓰는이유 컴포넌트간 state공유가 편해짐
 export default configureStore({
   reducer: { 
-    
+    // 작명 : 위에 정해둔 변수.reducer
+    user : user.reducer,
+    buy : buy.reducer
   }
 }) 
